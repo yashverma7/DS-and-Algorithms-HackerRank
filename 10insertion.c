660f4f961cc3022b36ac45406120fbfe76a6405d
Yash Verma <50752496+yashverma7@users.noreply.github.com>
2021-06-24 23:04:35 +0530

    Add files via upload

---
+++ 10insertion.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+//display function to print elements in array
+void display(int arr[],int n){
+    //Traversal
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+}
+
+indInsertion(int arr[],int size,int element, int capacity,int index){
+//insertion
+//if size of arr is greater than the capacity then we cant insert the element
+
+    if (size>=capacity){
+        return  -1;
+    }
+//shifting element
+    for (int i=size-1; i>=index;i--)
+    {
+        arr[i+1]=arr[i];
+    }
+
+//inserting the new element
+    arr[index]=element;
+    return 1;
+}
+
+
+int main(){
+    int arr[100]={1,2,6,78};
+    int size=4, element=45,index=3;
+    display(arr,size);
+    indInsertion(arr,size,element,100,index);
+    size+=1;
+    display(arr,size);
+    return 0;
+}
